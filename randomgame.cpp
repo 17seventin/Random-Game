8262818b100ae83e255a23edc62edc10bdb2a5ba
17seventin <116292119+17seventin@users.noreply.github.com>
2023-09-22 23:31:58 +0000

    Update randomgame.cpp

--- randomgame.cpp
+++ randomgame.cpp
@@ -11,6 +11,7 @@ int main()
 	srand(time(NULL));
 	// random number ( 1 - 5 )
 	int rand_num = 1 + rand() % 5;
+	string sure_replay;
 	// user number choose
 	int user_num;
 	// user tries
@@ -24,19 +25,28 @@ int main()
 		// user input number
 		cin >> user_num;
 		// if user pick some wrong number, then code will start
-		if (user_num != rand_num){
+		if (user_num != rand_num) {
 			cout << "Wrong number.\n";
-		// if user pick some wrong number, tries gone -1
-		tries--;
-		cout << "Tries: " << tries;
-		// if tries out, then game end
-		if (tries == 0) {
-			cout << "\nTries out. \nYou lose!";
-			return 0;
-		}}
+			// if user pick some wrong number, tries gone -1
+			tries--;
+			cout << "Tries: " << tries;
+			// if tries out, then game end
+			if (tries == 0) {
+				cout << "\nTries out. \nYou lose!\n";
+				cout << "Do you wanna replay?\n-> ";
+				cin >> sure_replay;
+				if (sure_replay == "yes") {
+					main();
+				}
+				else if (sure_replay == "no") {
+					return 0;
+				}
+			}
+			
+		}
 		// if user pick guess number, stop = true, also we have while(!true) that mean if stop = true next code will start
 		else stop = true;
-	} while(!stop);
+	} while (!stop);
 	cout << "You win!";
 	// enjoy ( ig )
 }
